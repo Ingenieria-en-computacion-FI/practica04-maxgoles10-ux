2286e2d74f7ceddadde7b937caff6cda62a90ad7
Maximiliano Marin Avila <maxgoles10@gmail.com>
2026-03-12 21:26:23 -0600

    realloc.c
    En este programa usamos realloc para aumentar y/o reducir la memoria

--- examples/realloc.c
+++ examples/realloc.c
@@ -3,28 +3,40 @@
 
 
 int main (){
-  // TODO(1): Declara dos apuntadores a entero llamados array, array2
-  // TODO(2): Declara una variable para indicar cuantos elementos se requieren
-  
-  printf("¿Cuantos elementos tiene el conjunto?\n");
-  // TODO(3): Lee el numero de elementos
-  // TODO(4): Usando una llamada a malloc reserva la memoria para array
-  
+ int *array, *array2;
+ int num;
+
+ printf("¿Cuantos elementos tiene el conjunto?\n");
+ scanf("%d", &num);
+ array = (int*)malloc(num*sizeof(int));
+
   if (array!=NULL) {
-    // TODO(5): Usando un ciclo for inserta los datos en el arreglo
+    for(int i=0; i<num; i++);{
+    printf("Ingresa el valor para el arreglo");
+    scanf("%d", (array+i));
+    }
+
     printf("[ ");
-    // TODO(6): Usando un ciclo for imprime la información en el arreglo
+    for(int=0; i<num; i++){
+      print("%d", *(array+i));
+    }
+
     printf("]\n");
 
-    // TODO(7): Asigna a array2, el arreglo duplicando su tamaño usando realloc
+    array2=(int*)realloc(array,2*num*sizeof(int));
      if (array2!=NULL) {
-        // TODO(8): Usando un ciclo for inserta los datos faltantes en el arreglo
+        for(int i=num; i<2*num; i++){
+          printf("Ingresa el valor para el arreglo: ");
+          scanf("%d", (array2+i));
+        }
         printf("[ ");
-        // TODO(9): Usando un ciclo for imprime la información en el arreglo
+        for(int=0; i<2*num; i++){
+          printf("%d", *(array2+i));
+        }
         printf("]\n");
       }
     
-    // TODO(10): libera la memoria reservada por realloc
+    free(array2);
   }
   return 0;
 }
